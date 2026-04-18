dd1c2aa1df8f9e271f7446c833e3f1788128b03c
HasanAgitUnal <hasanagitunal@proton.me>
2026-04-18 21:18:47 +0300

    Sounds: Removed desktop-logout sound

--- scripts/power.sh
+++ scripts/power.sh
@@ -10,15 +10,12 @@ case $ACTION in
         systemctl suspend 
         ;;
     logout) 
-        canberra-gtk-play -i desktop-logout & 
         sleep 0.4 && hyprctl dispatch exit 
         ;;
     reboot) 
-        canberra-gtk-play -i desktop-logout & 
         sleep 0.4 && reboot 
         ;;
     shutdown) 
-        canberra-gtk-play -i desktop-logout & 
         sleep 0.4 && shutdown now 
         ;;
 esac
