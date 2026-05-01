42e74c3cad5595cc906aa3b62e877a821a925592
HasanAgitUnal <hasanagitunal@proton.me>
2026-05-01 20:27:29 +0300

    bug fixes and small keybind config

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -203,7 +203,25 @@ require('lazy').setup({
                                         {
                                                 'saghen/blink.cmp',
                                                 version = '*',
+                                                build = function() require('blink.cmp').build():wait(60000) end,
                                                 opts = {
+                                                        keymap = {
+                                                                preset = 'none',
+                                                                ['<A-CR>'] = { 'select_and_accept', 'fallback' },
+                                                                ['<Tab>'] = { 'select_next', 'fallback' },
+                                                                ['<S-Tab>'] = { 'select_prev', 'fallback' },
+                                                                ['<A-Space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+                                                        },
+                                                        cmdline = {
+                                                                enabled = true,
+                                                                keymap = {
+                                                                        preset = 'none',
+                                                                        ['<A-CR>'] = { 'select_and_accept', 'fallback' },
+                                                                        ['<Tab>'] = { 'select_next', 'fallback' },
+                                                                        ['<S-Tab>'] = { 'select_prev', 'fallback' },
+                                                                        ['<A-Space>'] = { 'show', 'fallback' },
+                                                                },
+                                                        },
                                                         appearance = {
                                                                 use_nvim_cmp_as_default = true,
                                                                 nerd_font_variant = 'mono'
