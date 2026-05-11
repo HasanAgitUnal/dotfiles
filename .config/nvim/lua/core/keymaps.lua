c40dd6ae3b9382dfa2f92a6e69de67c0ff617715
HasanAgitUnal <hasanagitunal@proton.me>
2026-05-11 19:06:36 +0300

    Termux: extra keys row improved

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -9,15 +9,15 @@ vim.keymap.set('t', '<esc><esc>', '<c-\\><c-n>', { desc = 'exit terminal mode' }
 vim.keymap.set('n','<leader>e', '<cmd>NvimTreeToggle<cr>', { desc = 'open fil[e] tree', silent = true })
 
 -- windows
-vim.keymap.set('n', '<c-h>', '<c-w><c-h>', { desc = 'move focus to the left window' })
-vim.keymap.set('n', '<c-l>', '<c-w><c-l>', { desc = 'move focus to the right window' })
-vim.keymap.set('n', '<c-j>', '<c-w><c-j>', { desc = 'move focus to the lower window' })
-vim.keymap.set('n', '<c-k>', '<c-w><c-k>', { desc = 'move focus to the upper window' })
+vim.keymap.set('n', '<M-h>', '<c-w>h', { desc = 'move focus to the left window' })
+vim.keymap.set('n', '<M-l>', '<c-w>l', { desc = 'move focus to the right window' })
+vim.keymap.set('n', '<M-j>', '<c-w>j', { desc = 'move focus to the lower window' })
+vim.keymap.set('n', '<M-k>', '<c-w>k', { desc = 'move focus to the upper window' })
 
-vim.keymap.set("n", "<c-s-h>", "<c-w>h", { desc = "move window to the left" })
-vim.keymap.set("n", "<c-s-l>", "<c-w>l", { desc = "move window to the right" })
-vim.keymap.set("n", "<c-s-j>", "<c-w>j", { desc = "move window to the lower" })
-vim.keymap.set("n", "<c-s-k>", "<c-w>k", { desc = "move window to the upper" })
+vim.keymap.set("n", "<M-s-h>", "<c-w>H", { desc = "move window to the left" })
+vim.keymap.set("n", "<M-s-l>", "<c-w>L", { desc = "move window to the right" })
+vim.keymap.set("n", "<M-s-j>", "<c-w>J", { desc = "move window to the lower" })
+vim.keymap.set("n", "<M-s-k>", "<c-w>K", { desc = "move window to the upper" })
 
 -- debugging
 vim.keymap.set("n", "<leader>d", "", { desc = "[D]ebug" })
