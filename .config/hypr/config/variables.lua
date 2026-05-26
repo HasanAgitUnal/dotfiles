8e4c0e22261948e7cc8cf31861c417ba11d302a7
HasanAgitUnal <hasanagitunal@proton.me>
2026-05-26 19:21:46 +0300

    Hyprland: Fixed error with rogue startup

--- .config/hypr/config/variables.lua
+++ .config/hypr/config/variables.lua
@@ -18,7 +18,7 @@ APPS = {
         -- TUI apps
         gemini = terminal .. " --title 'Gemini' --working-directory /tmp/gemini --app-id gemini gemini",
         notes  = terminal .. " --title 'Notes' --app-id notes --working-directory ~/documents/obsidian nvim TODOS.md",
-        rogue  = terminal .. " --title 'Rogue' -o tweak.font-monospace-warn=no -o main.font='square:size=12.5' sh -c 'if [ -f ~/rogue.save ]; then rogue -r; else rogue; fi'"
+        rogue  = terminal .. " --title 'Rogue' -o tweak.font-monospace-warn=no -o main.font='square:size=12.5' sh -c 'sleep 0.2; if [ -f ~/rogue.save ]; then rogue -r; else rogue; fi'"
 }
 
 SOUND_THEME = HOME .. "/.local/share/sounds/modern-minimal-ui-sounds"
